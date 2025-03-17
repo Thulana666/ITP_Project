77d3396c25248261fe5784aa16685fc6851e1cc5
Thulana666 <thulanads@gmail.com>
2025-03-17 20:26:49 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Sample Route (Test if backend works)
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
+
+// Server Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
