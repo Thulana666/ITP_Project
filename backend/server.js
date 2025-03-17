dd47fb3f910495b9886522a1d9560f965ea6117e
Thulana666 <thulanads@gmail.com>
2025-03-17 20:46:17 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const paymentRoutes = require("./Routes/PaymentRoutes");
 
 // Connect to MongoDB
 connectDB().then(() => {
-  console.log("MongoDB connected successfully");
+  console.log("MongoDB connected successfully!");
 }).catch(err => {
   console.error("MongoDB connection error:", err);
   process.exit(1); // Exit process if database connection fails
